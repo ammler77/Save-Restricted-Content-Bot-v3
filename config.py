a628949e7c6efb916852664c5fbd48f0b8b4cc9a
ammler77 <mohamedazi3ed@gmail.com>
2025-05-23 00:05:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,36 +2,6 @@
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
-
-INST_COOKIES = """
-# wtite up here insta cookies
-"""
-
-YTUB_COOKIES = """
-# write here yt cookies
-"""
-
-API_ID = os.getenv("21707705", "")
-API_HASH = os.getenv("317937fef1c8eecaacca7b7bb16174c9", "")
-BOT_TOKEN = os.getenv("7672816193:AAFlIlNEgBhy31XSUeZBb1aLwYhBosmVep4", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("8096095996", "").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-4984574105")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002464242518")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
-YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
-INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
+import os for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
 
